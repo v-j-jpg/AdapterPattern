3b19c32adfe38758c4d56c3bbfa720b6cc5fad93
“v-j-jpg” <jovanavujanic89@gmail.com>
2020-10-20 14:07:56 +0200

    First commit

---
+++ Zivotinje/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZivotinjskoCarstvo;
+
+namespace Zivotinje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { TableEntity=
+
+            Konj l1 = new Konj();
+            Svinja l2 = new Svinja();
+
+            Labud labud = new Labud();
+            Vrabac vrabac = new Vrabac();
+
+            Adapter l3 = new Adapter(labud);
+            Adapter l4 = new Adapter(vrabac);
+
+
+            l1.OglasiSe();
+            l3.OglasiSe();
+            l4.OglasiSe();
+            l2.OglasiSe();
+
+      
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
